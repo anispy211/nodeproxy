64a0ace65a2d99aaeab09e808d6a20e6b27cb6d7
anispy211 <anispy211@gmail.com>
2024-11-03 20:18:19 +0530

    vercel json

--- index.js
+++ index.js
@@ -11,7 +11,4 @@ app.get('/proxy', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
